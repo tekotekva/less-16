980601d4b2953695a2a87f74dc8000fc44cf677f
tekvatekva <165680892+tekvatekva@users.noreply.github.com>
2024-07-23 11:22:14 +0400

    all tasks is done

--- JS/script.js
+++ JS/script.js
@@ -18,13 +18,13 @@ for (let index = 0; index < array8.length; index++) {
 }
 
 //  version 2
+let array8 =  [ [2, -3, 5, 11], [1, -35, -11], [12, -36, -24] ];
 
 for ( let item of array8 ) {
     for (let element of item) {
         if (element > 0) {
             console.log(element);
-        }
-        
+        }    
     }
 }   
 
@@ -110,6 +110,64 @@ console.log(result);
 
 // task #5
 
-let array = [1,2,3,4,5];
+let array1 = [1,2,3,4,5];
+
+let reversed = []
+for (let i = array1.length - 1; i >= 0; i--){
+    reversed.push(array1[i])
+}
+console.log(reversed);
+
+// let numbers_array = [1, 2, 3, 4, 5];
+// numbers_array.reverse();
+// console.log(numbers_array);
 
 
+// task #4
+
+let number = 7
+
+if(number % 2 == 0) {
+    console.log("The number is even.");
+}
+else {
+    console.log("The number is odd.");
+}
+
+
+// task #3
+
+let array9= [ 10, 142, 542, 8410, 383]
+console.log(Math.max(...array9));
+
+// task #2
+
+let user = {
+    firstname: 'giorgi',
+    lastname: 'saakadze',
+    age: 32,
+    isloggedin: true
+  }
+
+  function formatName(user) {
+    if (user.isloggedin) {
+      return `${user.firstname} ${user.lastname}`;
+    } else {
+      return false;
+    }
+  }
+  console.log(formatName(user));
+
+
+  // task #1
+
+  function sum(...numbers) {
+    let numberSum = 0;
+    for (let item of numbers) {
+        numberSum += item;
+    }
+    return numberSum;
+  }
+
+  let resultSum = sum(10, 50, 6, 7, 8, 11, 6, 3, 9);
+  console.log(resultSum);
